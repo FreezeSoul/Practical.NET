415d266e86015f8600a3d6a7faac4b785a2eb010
Phong Nguyen <nguyendoanphong.it@gmail.com>
2025-05-10 18:49:27 +0700

    [.Net 10] Sever-Sent Events

---
+++ examples/.Net 10/ServerSentEvents/ServerSentEvents/Program.cs
@@ -0,0 +1,18 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/sse", (CancellationToken cancellationToken) =>
+{
+    async IAsyncEnumerable<DateTimeOffset> GetTime([EnumeratorCancellation] CancellationToken cancellationToken)
+    {
+        while (!cancellationToken.IsCancellationRequested)
+        {
+            yield return DateTimeOffset.UtcNow;
+            await Task.Delay(2000, cancellationToken);
+        }
+    }
+
+    return TypedResults.ServerSentEvents(GetTime(cancellationToken), eventType: "dateTime");
+});
+
+app.Run();
